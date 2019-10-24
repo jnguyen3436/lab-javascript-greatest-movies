a8f21a53b86425c31f955701cf37d439452b9970
jnguyen3436 <jnguyen3436@gmail.com>
2019-10-24 17:56:24 -0400

    sorta done? I'm pretty sure I'm done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,8 +1,8 @@
 function ratesAverage(movies) {
-    const totalRate = movies.reduce((sum, elem) => {
-        return sum += Number(elem.rate);
+    const totalRate = movies.reduce((sum, eachMovie) => {
+        return sum += Number(eachMovie.rate);
     }, 0);
-
+//Found Average and made sure it was rounded
     const averageRate = Math.round(totalRate / movies.length * 100) / 100;
     return averageRate;
 }
@@ -10,20 +10,19 @@ function ratesAverage(movies) {
 
 
 // Iteration 2: Drama movies - Get the average of Drama Movies
-function dramaMoviesRate(movie) {
-    const dramaMovies = movie.filter((x) => {
-        return x.genre.includes('Drama');
-    })
-    // console.log(ratesAverage(dramaMovies));
-    let average = ratesAverage(dramaMovies);
-    return isNaN(average) ? 0 : average;
-
-    // or 
-    // let average = Number(ratesAverage(dramaMovies).toFixed(2));
-    // return average;
-
-    
+//fills new array if it has drama in the genre
+function dramaMoviesRate(movies) {
+    let isDrama = movies.filter(eachMovie => {
+        return eachMovie.genre.indexOf('Drama') === 0;
+    });
+//checks if Array is empty
+    if (isDrama.length === 0) {
+        return 0;
+    }
+//gets average rate of just drama videos
+    return ratesAverage(isDrama);
 }
+//ORDER MOVIES BY YEAR
 function orderByYear(movies){
     sortedMovies = movies.sort(function(a,b){
       if (a.year !== b.year) {
@@ -34,44 +33,22 @@ function orderByYear(movies){
     })
     return sortedMovies
     }
-// Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
-function orderByDuration(movie) {
-    const duration = movie.sort((a, b) => {
-        //         return b - a;
-        if (a.duration < b.duration) {
-            return -1;
-        }
-        if (a.duration > b.duration) {
-            return 1;
-        }
-        if (a.title < b.title) {
-            return -1
-        }
-        if (a.title > b.title) {
-            return 1
-        }
-        return 0;
-    });
-    return duration;
-}
 
-// Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
-
-function howManyMovies(param) {
-    const filteredSS = param.filter(elem => elem.director.includes('Steven Spielberg'));
-    const dramaSS = filteredSS.filter(elem => elem.genre.includes('Drama'));
+function howManyMovies(movies) {
+    const filteredSS = movies.filter(eachMovie => eachMovie.director.includes('Steven Spielberg'));
+    const dramaSS = filteredSS.filter(eachMovie => eachMovie.genre.includes('Drama'));
     return dramaSS.length;
 }
 
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 
-function orderAlphabetically(param) {
-    const movieTitle = param.map(anything => anything.title)
+function orderAlphabetically(movies) {
+    const movieTitle = movies.map(anything => anything.title)
 
     const sortTitle = movieTitle.sort().splice(0, 20);
     return sortTitle;
-}
+}-
 
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
@@ -108,12 +85,68 @@ function turnHoursToMinutes(time) {
 }
 
 //BONUS
-function bestYearAvg(arr){
-const years = arr.map((eachMovie) =>{
-    eachMovie = Object.assign({}, eachMovie);
-    // console.log(eachMovie)
-    const t = eachMovie.duration
-}
-else
-return null
-}
+function bestYearAvg(movies){
+    //Sort by year
+    if (movies.length === 0)
+    {
+      return null;
+    }
+    let yearSortedArr = movies.sort(function(a,b){
+      return Number(a.year - b.year);
+    });
+    //Split sorted array
+    let splitYear = [];
+    for (i = 0; i < 250; i++){
+      splitYear.push([]);
+    }
+    let count = 0;
+    let year = yearSortedArr[0].title
+    let index = 0;
+    previousYear = 0;
+    while (count < yearSortedArr.length)
+    {
+      if (yearSortedArr[count].year != previousYear)
+      {
+        index++;
+        previousYear = yearSortedArr[count].year;
+      }
+      splitYear[index].push(yearSortedArr[count])
+      count++;
+    }
+    splitYear.splice(0,1);
+    //Splice empty array values off of end of sorted array
+    for (i = 0; i < splitYear.length; i++)
+    {
+      if(splitYear[i].length === 0)
+      {
+        splitYear = splitYear.splice(0, i)
+      }
+    }
+    let sum = 0;
+    let scoreArray = [];
+    //Sum score on each split array
+    for (i = 0; i < splitYear.length; i++)
+    {
+      sum = 0;
+      for (j = 0; j < splitYear[i].length; j++)
+      {
+        sum += Number(splitYear[i][j].rate);
+      }
+      scoreArray.push(sum);
+    }
+    //Divide score by length of array, push to new array of scores
+    let avgArray = scoreArray.map(function(sum, i){
+      return sum / splitYear[i].length;
+    });
+    //Find max score in array
+    let max = 0;
+    index = 0;
+    avgArray.forEach(function(score, i){
+      if (score > max){
+        max = score;
+        index = i;
+      }
+    });
+    //Associate it with year in second array
+    return "The best year was " + splitYear[index][0].year + " with an average rate of " + max;
+  }
